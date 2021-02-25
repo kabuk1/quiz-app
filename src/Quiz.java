e510885d817427183a9a9677ae0cac6901894105
kabuk1 <leslie@agentodigital.com>
2021-02-25 17:00:08 +0000

    added ask and check methods

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,9 +1,14 @@
 import javax.swing.JOptionPane;
 
 public class Quiz {
+	
+	static int nQuestions = 0;
+	static int nCorrect = 0;
+	
+	static String question;
 
 	public static void main(String[] args) {
-		// Create strings for question and choices
+		// question 1
 		String question = "Which mascot represents the Philadelphia 76ers?\n";
 		question += "A. Phillie Phanatic\n";
 		question += "B. Gritty\n";
@@ -11,26 +16,58 @@ public class Quiz {
 		question += "D. Swoop\n";
 		question += "E. Phang\n";
 		
+		check(question, "C");
+		
+		// question 2
+		question = "Which professional sports team in Philadelphia were known as the Broad Street Bullies?\n";
+		question += "A. Flyers\n"; // correct answer
+		question += "B. Eagles\n";
+		question += "C. 76ers\n";
+		question += "D. Union\n";
+		question += "E. Phillies\n";
+		
+		check(question, "A");
+		
+		// question 3
+		question = "In the film Invincible, which Philadelphia team did Vince Papale play for?\n";
+		question += "A. Phillies\n";
+		question += "B. Flyers\n";
+		question += "C. Union\n"; 
+		question += "D. Eagles\n"; //correct answer
+		question += "E. 76ers\n";
+		
+		check(question, "D");
+
+		JOptionPane.showMessageDialog(null, nCorrect + " correct out of " + nQuestions + ".");
+		
+	}
+	
+	// method asks question until a valid answer is received
+	static String ask(String question) {
+		
 		while (true) {
-			String answer = JOptionPane.showInputDialog(question);
-			answer = answer.toUpperCase();
+			String answer = JOptionPane.showInputDialog(question).toUpperCase();
 			
 			// Check for valid input
-			if (answer.equals("A") || answer.equals("B") || answer.equals("C") || answer.equals("D") || answer.equals("E")) {
-				// If valid answer, check if answer is correct
-				if (answer.equals("C")) {
-					JOptionPane.showMessageDialog(null,"Correct!");
-					break;
-					
-				} else {
-					JOptionPane.showMessageDialog(null,"Incorrect. Please try again.");
-					continue;
-					
-				}
-			} else {
-				// Notify user of invalid answer
-				JOptionPane.showMessageDialog(null,"Invalid answer. Please enter A, B, C, D or E.");
-			}
+			boolean valid = (answer.equals("A") || answer.equals("B") || answer.equals("C") || answer.equals("D") || answer.equals("E"));
+			if (valid) return answer; 
+			// Notify user of invalid answer
+			JOptionPane.showMessageDialog(null,"Invalid answer. Please enter A, B, C, D or E.");
 		}
 	}
+	
+	// method checks if the answer is correct
+	static void check (String question, String correctAnswer) {
+		nQuestions ++; // increment question
+		String answer = ask(question);
+		
+		if(answer.equals(correctAnswer)) {
+			nCorrect ++; // increment correct answers
+			JOptionPane.showMessageDialog(null,"Correct!");	
+		} else {
+			JOptionPane.showMessageDialog(null,"The correct answer is " + correctAnswer + ".");	
+		}
+		
+	}
+	
 }
