c5ab799fdf7c0eee3f1973c84b2389d3d5c33bce
kabuk1 <leslie@agentodigital.com>
2021-03-29 12:59:34 +0100

    Refactored Question class to use atw & swing

--- src/Question.java
+++ src/Question.java
@@ -1,31 +1,47 @@
-import javax.swing.JOptionPane;
+import java.awt.*;
+import javax.swing.*;
 
 public abstract class Question {
-	// class variables
 	static int nQuestions = 0;
 	static int nCorrect = 0;
 	
-	// instance variables
-	String question;
+	QuestionDialog question;
 	String correctAnswer;
 	
-	// abstract declaration
-	abstract String ask();
+	//constructor that initializes instance variable "question"
+	public Question(String question) {
+		this.question = new QuestionDialog();
+		this.question.setLayout(new GridLayout(0,1)); //single-column grid layout,
+		this.question.add(new JLabel(" "+question+" ",JLabel.CENTER));  //adds a text label 
+	}
 	
-	// instance method "check"
+	// method "initQuestionDialog" 
+	void initQuestionDialog() {
+		this.question.setModal(true);  //instance variable "question" modal
+		this.question.pack();  //sets its size with "pack"
+		this.question.setLocationRelativeTo(null);  //positions it in the center of the screen
+	}
+
+	// "ask" method
+	String ask() {
+		question.setVisible(true);  //instance variable "question" visible 
+		return question.answer;  //returns the value "question.answer"
+	}
+	
 	void check () {
-		nQuestions ++; //increment question
+		nQuestions ++;
+		// calls the "ask" method
 		String answer = ask();
 		
 		if (answer.equals(correctAnswer)) {
-			nCorrect ++; // increment correct answers
+			nCorrect ++;
 			JOptionPane.showMessageDialog(null,"Correct!");
 		} else {
 			JOptionPane.showMessageDialog(null,"Incorrect. The correct answer is " + correctAnswer + ".");
 		}
 			
 	}
-	// class method "showResults"
+	
 	static void showResults() {
 		JOptionPane.showMessageDialog(null, nCorrect + " correct out of " + nQuestions + ".");
 	}
